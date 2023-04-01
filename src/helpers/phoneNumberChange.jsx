255d88da350064852f309ae1370aa948d2b81ff5
EzequielFerreras2 <ezequielferreras2@gmail.com>
2023-04-01 10:31:59 -0400

    Changes Formater

--- src/helpers/phoneNumberChange.jsx
+++ src/helpers/phoneNumberChange.jsx
@@ -5,14 +5,14 @@ export const phoneNumberChange =(data)=>{
     // var phoneNumber =`(${firts})-${middel}-${last}`
     // return phoneNumber.toString();
      //Filter only numbers from the input
-  let cleaned = ('' + data).replace(/\D/g, '');
+  let plain = ('' + data).replace(/\D/g, '');
   
   //Check if the input is of correct length
-  let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  let fixed = plain.match(/^(\d{3})(\d{3})(\d{4})$/);
 
-  if (match) {
-    return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+  if (fixed) {
+    return '(' + fixed[1] + ')-' + fixed[2] + '-' + fixed[3]
   };
 
-  return null
+  return "Numero incorrecto"
   };
